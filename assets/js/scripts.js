08663730126e0a3b845fa4f1044edf34c47ea6b9
Duncan CI <duncan.thow@students.codeinstitute.net>
2024-06-21 12:44:09 +0000

    Code for incrementing scores added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -48,12 +48,12 @@ function checkAnswer() {
 
         if (isCorrect) {
             alert("Hey! you got it right! :D");
+            incrementScore();
         }   else {
             alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+            incrementWrongAnswer();
         }
-
     runGame(calculatedAnswer[1]);
-
 }
 
 /**
@@ -74,11 +74,22 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments by 1
+ */
 function incrementScore() {
+    
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current incorrect score from the DOM and increments by 1
+ */
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
