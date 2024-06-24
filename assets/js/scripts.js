fe8e4a687520daef7c0dfa3f7d58ea542f78eeab
Duncan CI <duncan.thow@students.codeinstitute.net>
2024-06-24 11:20:42 +0000

    Division added non-functional

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -47,6 +47,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2)
     }   else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2)
+    }   else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2)
     }   else {
         alert (`unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -84,11 +86,13 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
+    }  else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
+    }  else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    }  else if (operator === "-") {
+        return [operand1 - operand2, "division"];
+    }  else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, Aborting!`;
     }
@@ -137,4 +141,11 @@ function displayMultiplyQuestion(operand1, operand2 ) {
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2 ) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1
+    document.getElementById("operator").textContent = "/";
 }
